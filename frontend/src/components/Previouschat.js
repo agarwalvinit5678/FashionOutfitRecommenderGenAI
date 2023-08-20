c0efc0b5ca3f7148e4295e1d13564ce3c60ad4d7
agarwalvinit5678 <agarwalvinit5678@gmail.com>
2023-08-20 16:32:41 +0530

    frontend2.0

--- frontend/src/components/Previouschat.js
+++ frontend/src/components/Previouschat.js
@@ -1,18 +1,26 @@
+// Import the necessary CSS styles
 import '../index.css';
 
+// Define the PreviousChat component for displaying chat text
 function PreviousChat(props) {
+  // Split the text into parts based on 'You:' and 'AI:' markers
   const textParts = props.text.split(/(You:|AI:)/);
 
   return (
+    // Chat container with conditional class for prompt text
     <div className={`chatt ${props.isPrompt ? 'prompt-text' : ''}`} style={{ whiteSpace: 'pre-line' }}>
+      {/* Map through text parts */}
       {textParts.map((part, index) =>
+        // Check if part is 'You:' or 'AI:' marker
         part === 'You:' || part === 'AI:' ? (
+          // Display marker with bold styling
           <strong key={index} style={{ fontWeight: 'bold' }}>
             {part}
           </strong>
         ) : (
+          // Display chat text
           <>
-          &nbsp;
+            &nbsp; {/* Add space before text */}
             {/* <br /> Add a newline before text */}
             {part}
           </>
@@ -22,4 +30,5 @@ function PreviousChat(props) {
   );
 }
 
+// Export the PreviousChat component
 export default PreviousChat;
--- frontend/src/components/ProductBox.js
+++ frontend/src/components/ProductBox.js
@@ -1,30 +1,44 @@
+// Import necessary libraries and CSS styles
 import React, { useState } from 'react';
 import '../index.css';
 import Products from './Products';
 
+// Define the ProductBox component for displaying different product collections
 function ProductBox(props) {
+  // State to track the active collection index
   const [activeCollection, setActiveCollection] = useState(0);
 
+  // Handler for navigating to the previous collection
   const handlePrevCollection = () => {
     setActiveCollection(prevIndex => (prevIndex > 0 ? prevIndex - 1 : props.products.length - 1));
   };
 
+  // Handler for navigating to the next collection
   const handleNextCollection = () => {
     setActiveCollection(prevIndex => (prevIndex < props.products.length - 1 ? prevIndex + 1 : 0));
   };
 
+  // Get data for the currently active collection
   const activeCollectionData = props.products[activeCollection];
   
   return (
+    // Container for displaying product collections
     <div className='productsbox'>
+      {/* Navigation buttons and collection title */}
       <div className='butbox'>
         <button className="navbut" onClick={handlePrevCollection}>&#8249;</button>
         <h2>Collection {activeCollection + 1}</h2>
         <button className="navbut" onClick={handleNextCollection}>&#8250;</button>
       </div>
+      
+      {/* Map through categories in the active collection */}
       {Object.keys(activeCollectionData).map((categoryKey, categoryIndex) => (
         <div key={categoryIndex}>
-          <h3>{categoryKey === 'Top' ? 'Topwear' : categoryKey === 'Bottom' ? 'Bottomwear' : categoryKey === 'Foot' ? 'Footwear' : 'Accessories'}</h3>
+          {/* Display category title based on categoryKey */}
+          <h3>
+            {categoryKey === 'Top' ? 'Topwear' : categoryKey === 'Bottom' ? 'Bottomwear' : categoryKey === 'Foot' ? 'Footwear' : 'Accessories'}
+          </h3>
+          {/* Display products within the category */}
           <Products products={activeCollectionData[categoryKey]}></Products>
         </div>
       ))}
@@ -32,4 +46,5 @@ function ProductBox(props) {
   );
 }
 
+// Export the ProductBox component
 export default ProductBox;
